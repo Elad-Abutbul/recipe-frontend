fa1c753decf6b9f45fa571eb6ce07db432754568
Elad Abutbul <eladabutbul61@gmail.com>
2023-12-28 10:09:53 +0200

    constants all the queryKey

--- src/Hooks/useQueryMutaion/index.js
+++ src/Hooks/useQueryMutaion/index.js
@@ -12,6 +12,7 @@ import {
   changeRecipeStarsInComments,
   editComment,
 } from "../../Functions";
+import { QUERY_KEY } from "../../constants";
 
 const useQueryMutation = () => {
   const queryClient = useQueryClient();
@@ -24,56 +25,58 @@ const useQueryMutation = () => {
 
   const deleteSavedRecipeMutation = useMutation({
     mutationFn: async (userId) => deleteSavedRecipe(userId),
-    onSuccess: () => queryClient.invalidateQueries(["savedRecipes"]),
+    onSuccess: () => queryClient.invalidateQueries([QUERY_KEY.SAVED_RECIPE]),
   });
 
   const saveRecipeseMutation = useMutation({
     mutationFn: async (recipeId) => saveRecipe(recipeId),
-    onSuccess: () => queryClient.invalidateQueries(["savedRecipes"]),
+    onSuccess: () => queryClient.invalidateQueries([QUERY_KEY.SAVED_RECIPE]),
   });
 
   const changeRecipeStarsInCommentsMutation = useMutation({
     mutationFn: async ({ userId, rating, recipeId }) =>
       changeRecipeStarsInComments(userId, rating, recipeId),
     onSuccess: (_, { recipeId }) =>
-      queryClient.invalidateQueries(["recipeComments", recipeId]),
+      queryClient.invalidateQueries([QUERY_KEY.RECIPE_COMMNETS, recipeId]),
   });
 
   const editCommentMutation = useMutation({
     mutationFn: async ({ commentId, comment }) =>
       editComment(commentId, comment),
     onSuccess: (_, { recipeId }) =>
-      queryClient.invalidateQueries(["recipeComments", recipeId]),
-  });
-
-  const deleteOwnerRecipeMutation = useMutation({
-    mutationFn: async (recipeId) => await deleteOwnerRecipe(recipeId),
-    onSuccess: () => queryClient.invalidateQueries(["allOwnerRecipes"]),
+      queryClient.invalidateQueries([QUERY_KEY.RECIPE_COMMNETS, recipeId]),
   });
 
   const addCommentMutation = useMutation({
     mutationFn: async ({ comment, recipeId }) =>
       await addComment(comment, recipeId),
     onSuccess: (_, { recipeId }) =>
-      queryClient.invalidateQueries(["recipeComments", recipeId]),
+      queryClient.invalidateQueries([QUERY_KEY.RECIPE_COMMNETS, recipeId]),
+  });
+
+  const deleteOwnerRecipeMutation = useMutation({
+    mutationFn: async (recipeId) => await deleteOwnerRecipe(recipeId),
+    onSuccess: () =>
+      queryClient.invalidateQueries([QUERY_KEY.ALL_OWNER_RECIPE]),
+  });
+
+  const editRecipeMutation = useMutation({
+    mutationFn: async ({ recipe, recipeId }) =>
+      await editRecipe(recipe, recipeId),
+    onSuccess: () =>
+      queryClient.invalidateQueries([QUERY_KEY.ALL_OWNER_RECIPE]),
   });
 
   const createRecipeMutation = useMutation({
     mutationFn: async (recipe) => await createRecipe(recipe),
-    onSuccess: () => queryClient.invalidateQueries(["home"]),
+    onSuccess: () => queryClient.invalidateQueries([QUERY_KEY.HOME]),
   });
 
   const changeRecipeStarsMutation = useMutation({
     mutationFn: async ({ userId, rating, recipeId }) =>
       await changeRecipeRating(userId, rating, recipeId),
     onSuccess: (_, { recipeId }) =>
-      queryClient.invalidateQueries(["recipe", recipeId]),
-  });
-
-  const editRecipeMutation = useMutation({
-    mutationFn: async ({ recipe, recipeId }) =>
-      await editRecipe(recipe, recipeId),
-    onSuccess: () => queryClient.invalidateQueries(["allOwnerRecipes"]),
+      queryClient.invalidateQueries([QUERY_KEY.RECIPE, recipeId]),
   });
 
   return {
---
+++ src/constants/queryKey/index.js
@@ -0,0 +1,7 @@
+export const QUERY_KEY = {
+  SAVED_RECIPE: "savedRecipes",
+  RECIPE_COMMNETS: "recipeComments",
+  ALL_OWNER_RECIPE: "allOwnerRecipes",
+  HOME: "home",
+  RECIPE: "recipe",
+};
